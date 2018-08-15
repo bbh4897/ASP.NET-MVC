176495be624447c15de961f27369b4573abffdbe
bbg <bagbrc@gmail.com>
2018-08-15 16:51:41 +0300

    PersonalUI db listeleme

--- PersonalUI/PersonalUI/Controllers/DepartmenController.cs
+++ PersonalUI/PersonalUI/Controllers/DepartmenController.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿using PersonalUI.Models.EntitityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace PersonalUI.Controllers
 {
     public class DepartmenController : Controller
     {
+
+        PersonelDbEntities ent = new PersonelDbEntities();
         // GET: Departmen
         public ActionResult Index()
         {
-            return View();
+            var model = ent.Departmen.ToList();
+            return View(model);
         }
     }
 }
