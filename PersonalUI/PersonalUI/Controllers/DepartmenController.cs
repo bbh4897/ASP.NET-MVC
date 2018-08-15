400bd8f62360caca32252f35379f4256d4cfe53f
bbg <bagbrc@gmail.com>
2018-08-15 16:07:51 +0300

    PersonalUI veritabanı eklendi ve navbar oluşturuldu

---
+++ PersonalUI/PersonalUI/Controllers/DepartmenController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalUI.Controllers
+{
+    public class DepartmenController : Controller
+    {
+        // GET: Departmen
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PersonalUI/PersonalUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PersonalUI.MvcApplication" Language="C#" %>
