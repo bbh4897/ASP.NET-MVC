56fac8031b56e7bb7c1821a366ae59a93cacbb30
bbg <bagbrc@gmail.com>
2018-09-01 00:05:52 +0300

    URl üzerinden json verisi getirme

---
+++ JsonGetData/JsonGetData/Scripts/custom.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+
+    $('#tableDepartmanlar').DataTable({
+        "language": {
+            "url": "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json"
+        }
+    });
+
+});
+
+
+
+
