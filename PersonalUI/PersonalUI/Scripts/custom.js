0086a0345b1bf04c18fe59d47a3a44cb604994b5
bbg <bagbrc@gmail.com>
2018-08-31 21:29:43 +0300

    jquery Datatables

---
+++ PersonalUI/PersonalUI/Scripts/custom.js
@@ -0,0 +1,9 @@
+$(function () {
+
+    $('#tableDepartmanlar').DataTable({
+        "language": {
+            "url": "http://cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json"
+        }
+    });
+
+});
