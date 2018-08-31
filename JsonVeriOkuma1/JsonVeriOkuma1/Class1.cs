0f00b9f8e3cd8cb79e048c2615bc2f893ab9ccc4
bbg <bagbrc@gmail.com>
2018-08-31 18:41:08 +0300

    Proje tamamlandı.

---
+++ JsonVeriOkuma1/JsonVeriOkuma1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JsonVeriOkuma1
+{
+    public class Class1
+    {
+        Int64 time;
+        Array states;
+
+        public Int64 Time { get => time; set => time = value; }
+        public Array States { get => states; set => states = value; }
+    }
+}
