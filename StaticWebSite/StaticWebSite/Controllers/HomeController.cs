d98c90a7328b311bb9e3d419c0e1cc711338aa03
bbg <bagbrc@gmail.com>
2018-09-25 13:05:35 +0300

    Bootstrap tema giydirme

---
+++ StaticWebSite/StaticWebSite/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaticWebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Hakkımızda")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Route("Ürünler")]
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        [Route("Mağaza")]
+        public ActionResult Store()
+        {
+            return View();
+        }
+    }
+}
